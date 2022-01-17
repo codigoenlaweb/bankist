1ca4ecb63962828c422f37b99b668e2fc66560a2
codigoenlaweb <codeenlaweb@gmail.com>
2022-01-17 19:50:21 -0400

    open acount with js

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
+/* query selector */
 const menu = document.querySelector('#menu-responsive');
 const overlay = document.querySelector('.overlay');
 const listMenu = document.querySelector('.nav__right-menu')
+const account = document.querySelector('#openAcount');
+const overlayAccount = document.querySelector('.modal__open-acount');
+
+/* end query selector */
 
 
-const open = () => {
-    listMenu.classList.remove('opacity0');
-    overlay.classList.remove('hidden')
+/* arrow fuction */
+
+// open the menu when touch
+const openaMenu = () => {
+    listMenu.classList.remove('hidden');
+    setTimeout( () => {
+        listMenu.classList.remove('opacity0');
+    }, 0)
+    overlay.classList.remove('hidden');
     
 } 
 
-const close = () => {
+// closed the menu when touch
+const closeMenu = () => {
     listMenu.classList.add('opacity0');
-    overlay.classList.add('hidden')
+    setTimeout( () => {
+        listMenu.classList.add('hidden');
+    }, 2500)
+    overlay.classList.add('hidden');
 }
 
-menu.addEventListener('click', open);
-overlay.addEventListener('click', close);
+const openacount = () => {
+    overlayAccount.classList.remove('hidden');
+    setTimeout( () => {
+        overlayAccount.classList.remove('opacity0');  
+    }, 0)
+    overlay.classList.remove('hidden');
+}
+
+//
+
+
+
+/* end arrow fuction */
+
+
+/* add event listener */
+menu.addEventListener('click', openaMenu);
+overlay.addEventListener('click', closeMenu);
+
+account.addEventListener('click', openacount);
+overlay.addEventListener('click', closeMenu);
+
+/* end add event listener */
