59b4089cd73cf5ea60adf399dc319d9afc97b3c6
codigoenlaweb <codeenlaweb@gmail.com>
2022-01-19 18:17:49 -0400

    form open acount

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* query selector */
 const menu = document.querySelector('#menu-responsive');
 const overlay = document.querySelector('.overlay');
+const overlayGray = document.querySelector('.overlay--gray');
 const listMenu = document.querySelector('.nav__right-menu')
 const account = document.querySelector('#openAcount');
 const overlayAccount = document.querySelector('.modal__open-acount');
@@ -10,7 +11,7 @@ const overlayAccount = document.querySelector('.modal__open-acount');
 
 /* arrow fuction */
 
-// open the menu when touch
+// open the menu when click
 const openaMenu = () => {
     listMenu.classList.remove('hidden');
     setTimeout( () => {
@@ -20,24 +21,32 @@ const openaMenu = () => {
     
 } 
 
-// closed the menu when touch
+// closed the menu when click
 const closeMenu = () => {
     listMenu.classList.add('opacity0');
     setTimeout( () => {
         listMenu.classList.add('hidden');
-    }, 2500)
+    }, 300)
     overlay.classList.add('hidden');
 }
 
+// open the form acount when click
 const openacount = () => {
     overlayAccount.classList.remove('hidden');
     setTimeout( () => {
         overlayAccount.classList.remove('opacity0');  
     }, 0)
-    overlay.classList.remove('hidden');
+    overlayGray.classList.remove('hidden');
 }
 
-//
+// closed the form acount when click overlay gray
+const closeForm = () => {
+    overlayAccount.classList.add('opacity0');
+    setTimeout( () => {
+        overlayAccount.classList.add('hidden');
+    }, 2500)
+    overlayGray.classList.add('hidden');
+}
 
 
 
@@ -49,6 +58,6 @@ menu.addEventListener('click', openaMenu);
 overlay.addEventListener('click', closeMenu);
 
 account.addEventListener('click', openacount);
-overlay.addEventListener('click', closeMenu);
+overlayGray.addEventListener('click', closeForm);
 
 /* end add event listener */
